9dee920575f3a96715c663125be6375b3e3add02
Augusto Lima Jardim <augustolimajardim@gmail.com>
2023-12-02 16:17:18 -0300

    Criação e conexão ao banco de dados com SQLite

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log(`Está escutando a porta ${PORT}`);
 });
 
+// DB connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("Conectou com sucesso");
+    })
+    .catch(err => {
+        console.log("Ocorreu um erro ao conectar: ", err);
+    });
+    
+
+// Routes
 app.get('/', (req, res) => {
     res.send("Está funcionando");
 })
